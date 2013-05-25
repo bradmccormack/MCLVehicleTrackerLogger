2b73575254d771a6893ad71eda249e9b1af2aebf
Brad McCormack <brad_mccormack@exemail.com.au>
2013-05-25 15:51:02 +1000

    - Fixing up missing css reference and grid

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
 	var map = L.map('Mainmap').setView([51.505, -0.09], 13);
 
+
 	// add an OpenStreetMap tile layer
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
 
 	// add a marker in the given location, attach some popup content to it and open the popup
 	L.marker([51.5, -0.09]).addTo(map) 
 	    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
 	    .openPopup();
+	    
 	
 });
