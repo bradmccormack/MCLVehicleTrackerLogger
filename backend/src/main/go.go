d3bc3bb28af4317fb8c6e24993023820fb85c0e2
Brad McCormack <bmccormack@ontech.com.au>
2013-05-30 17:39:51 +1000

    - listening and printing of incoming gps co-ord

--- backend/src/main/go.go
+++ backend/src/main/go.go
@@ -8,10 +8,6 @@ import (
 )
 
 
-func handleConnection() {
-}
-
-
 func main() {
 
     service := ":215";
@@ -22,35 +18,39 @@ func main() {
     }
     fmt.Printf("Listening on UDP Port %s", service)
 
-
-    con, err := net.ListenUDP("udp",udpAddr)
-    if(err != nil) {
-        fmt.Printf("Failed to create udp connection " + err.Error())
-        os.Exit(1)
+    for{
+        con, err := net.ListenUDP("udp",udpAddr)
+        if(err != nil) {
+            fmt.Printf("Failed to create udp connection - %s" , err)
+            os.Exit(1)
+        }
+        handleClient(con);
     }
-    defer con.Close();
+ }
 
     //palm off reading and writing to a go routine
-    go func(conn *net.UDPConn) {
-            var buff [512]byte
+func handleClient(conn *net.UDPConn) {
+        //defer conn.Close();
+        var buff [512]byte
+        var gps string
 
-            /*GPS input will look like
-             P L-34.50108,150.81094,S0.00,H147.2,D2013-05-26,T11:00:18,Ftrue,ISUS01
-            P= This is a position message, L is lattitude and longitude seperated by comma, S is Speed in KM/H H is heading in degrees
-            F is boolean fix or not D will be date and T for time  and I will be ID of logger
-            */
+        /*GPS input will look like
+         P L-34.50108,150.81094,S0.00,H147.2,D2013-05-26,T11:00:18,Ftrue,ISUS01
+        P= This is a position message, L is lattitude and longitude seperated by comma, S is Speed in KM/H H is heading in degrees
+        F is boolean fix or not D will be date and T for time  and I will be ID of logger
+        */
 
-            _, addr, err := conn.ReadFromUDP(buff[:])
-            if err != nil {
-                fmt.Printf("Error reading from UDP")
-            }
+        n, addr, err := conn.ReadFromUDP(buff[:])
+        if err != nil {
+            fmt.Printf("Error reading from UDP")
+        }
 
-            daytime := time.Now().String()
-            conn.WriteToUDP([]byte(daytime), addr)
-            fmt.Printf("Responded")
+        gps = string(buff[:n]);
 
-    }(con)
-
-
+        daytime := time.Now().String()
+        conn.WriteToUDP([]byte(daytime), addr)
+        fmt.Printf("Responded")
+        conn.Close();
 
 }
+
