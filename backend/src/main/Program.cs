b9ab100bc8dfa3b29a2bbec6be29791d889fff23
Brad McCormack <brad_mccormack@exemail.com.au>
2013-06-01 11:50:29 +1000

    Remove seperate time field and add sql lite logging

---
+++ backend/src/main/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace testwpf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dt = DateTime.Now.ToString("r").Replace(",", "");
+            Console.WriteLine(dt);
+
+            UdpClient udpClient = new UdpClient("127.0.0.1", 215);
+            Byte[] sendBytes = Encoding.ASCII.GetBytes("PHi there buddy,L-34.50108,150.81094,S0.00,H147.2,D" + dt + ",Ftrue,ISUS01");
+            try
+            {
+                udpClient.Send(sendBytes, sendBytes.Length);
+                Console.ReadKey();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
--- backend/src/main/go.go
+++ backend/src/main/go.go
@@ -1,131 +1,119 @@
-//https://github.com/mattn/go-sqlite3 Add this driver
-
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
-    "time"
-    "strings"
-    "strconv"
-    "database/sql"
-	//"database/sql/driver"
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
-type GPSRecord struct{
-    latitude string
-    longitude string
-    message string
-    speed int
-    heading float64
-    fix bool
-    date time.Time
-    time time.Time
-    ID string
+type GPSRecord struct {
+	latitude  string
+	longitude string
+	message   string
+	speed     int
+	heading   float64
+	fix       bool
+	date      time.Time
+	ID        string
 }
 
 func main() {
 
-    db, err := sql.Open("sqlite3", "backend.db")
-    if err != nil {
-        fmt.Printf("Cannot open backend.db . Exiting")
-        os.Exit(1)
-    }
-    defer db.Close()
+	db, err := sql.Open("sqlite3", "./backend.db")
+	if err != nil {
+		fmt.Printf("Cannot open backend.db . Exiting")
+		os.Exit(1)
+	}
+	defer db.Close()
 
+	service := ":215"
+	udpAddr, err := net.ResolveUDPAddr("udp4", service)
+	if err != nil {
+		fmt.Printf("Failed to resolve UDP address")
+		os.Exit(1)
+	}
+	fmt.Printf("Listening on UDP Port %s", service)
 
-    service := ":215";
-    udpAddr, err := net.ResolveUDPAddr("udp4", service);
-    if err != nil {
-        fmt.Printf("Failed to resolve UDP address")
-        os.Exit(1)
-    }
-    fmt.Printf("Listening on UDP Port %s", service)
-
-    for{
-        con, err := net.ListenUDP("udp",udpAddr)
-        if(err != nil) {
-            fmt.Printf("Failed to create udp connection - %s" , err)
-            os.Exit(1)
-        }
-        handleClient(con);
-    }
- }
-
-
-func notifyHTTP(entry *GPSRecord){
-     //http://html5labs.interoperabilitybridges.com/prototypes/websockets/websockets/info    -- WOOT it's possible to use web sockets on the client
+	for {
+		con, err := net.ListenUDP("udp", udpAddr)
+		if err != nil {
+			fmt.Printf("Failed to create udp connection - %s", err)
+			os.Exit(1)
+		}
+		handleClient(db, con)
+	}
 }
 
-func logEntry(entry *GPSRecord)  {
-    //WIP
-    /*
-    tx, err := db.Begin()
-    result, err = db.Exec("INSERT INTO GPSRecords (id, Message, Latitude, Longitude, Speed, Heading, Fix, DateTime, BusID) VALUES ( ? , ?, ? , ? , ? ,? ,? , ? , ?)",
-         entry.message,
-         entry.latitude,
-         entry.longitude,
-         entry.speed,
-         entry.heading,
-         entry.date,
-         entry.time,
-         entry.fix,
-         entry.ID)
-
-    if err != nil || result.RowsAffected == 0 {
-        fmt.Printf("Failed to insert row");
-    }
-
-    */
-     //daytime := time.Now().String()
+func notifyHTTP(entry *GPSRecord) {
+	//http://html5labs.interoperabilitybridges.com/prototypes/websockets/websockets/info    -- WOOT it's possible to use web sockets on the client
 }
 
-    //palm off reading and writing to a go routine
-func handleClient(conn *net.UDPConn) {
-        defer conn.Close();
-        var buff [512]byte
-        var entry GPSRecord
+func logEntry(db *sql.DB, entry *GPSRecord) {
 
-        n, addr, err := conn.ReadFromUDP(buff[:])
-        if err != nil {
-            fmt.Printf("Error reading from UDP")
-        }
+	_, err := db.Exec("INSERT INTO GPSRecords (Message, Latitude, Longitude, Speed, Heading, Fix, DateTime, BusID) VALUES ( ? , ?, ? , ? , ? ,? ,? , ?)",
+		entry.message,
+		entry.latitude,
+		entry.longitude,
+		entry.speed,
+		entry.heading,
+		entry.fix,
+		entry.date,
+		entry.ID)
 
-        gpsfields := strings.Split(string(buff[:n]), ",");
-        if len(gpsfields) != 9 {
-            fmt.Printf("Error. GPS fields length is incorrect. Is %d should be %d", len(gpsfields), 9)
-            os.Exit(1)
-        }
-        //All data is validated on the logger end so I'm going to assume for now that Parsing will be fine. Perhaps a network error could occur and I'll fix that up later
+	if err != nil {
+		fmt.Printf("Failed to insert row %s", err)
+	}
 
-        entry.message = gpsfields[0][1:]
-        entry.latitude = gpsfields[1][1:]
-        entry.longitude = gpsfields[2]
-        entry.speed, _ = strconv.Atoi(gpsfields[3][1:])
-        entry.heading, _  = strconv.ParseFloat(gpsfields[4][1:], 32)
-        entry.date, _ = time.Parse(  time.RFC822 ,gpsfields[5][2:])  //todo pull out just the date component and format
-        entry.time, _ = time.Parse(  time.RFC822 , gpsfields[6][1:]) //todo pull out just the time component and format
-        entry.fix = gpsfields[7][1:] == "true";
-        entry.ID = gpsfields[8]
-
-        fmt.Printf("Message %s Lat %s Long %s speed %d heading %f fix %t date %s time %s id %s\n",
-        entry.message,
-        entry.latitude,
-        entry.longitude,
-        entry.speed,
-        entry.heading,
-        entry.fix,
-        entry.date,
-        entry.time,
-        entry.ID)
-
-        go logEntry(&entry) //save to database
-        go notifyHTTP(&entry) //notify any HTTP observers
-
-        conn.WriteToUDP([]byte("OK"), addr)
-        fmt.Printf("Responded to %s", addr)
-        conn.Close();
-
+	//daytime := time.Now().String()
 }
 
+//palm off reading and writing to a go routine
+func handleClient(db *sql.DB, conn *net.UDPConn) {
+	defer conn.Close()
+	var buff [512]byte
+	var entry GPSRecord
+
+	n, addr, err := conn.ReadFromUDP(buff[:])
+	if err != nil {
+		fmt.Printf("Error reading from UDP")
+	}
+
+	gpsfields := strings.Split(string(buff[:n]), ",")
+	if len(gpsfields) != 8 {
+		fmt.Printf("Error. GPS fields length is incorrect. Is %d should be %d", len(gpsfields), 8)
+		os.Exit(1)
+	}
+	//All data is validated on the logger end so I'm going to assume for now that Parsing will be fine. Perhaps a network error could occur and I'll fix that up later
+
+	entry.message = gpsfields[0][1:]
+	entry.latitude = gpsfields[1][1:]
+	entry.longitude = gpsfields[2]
+	entry.speed, _ = strconv.Atoi(gpsfields[3][1:])
+	entry.heading, _ = strconv.ParseFloat(gpsfields[4][1:], 32)
+	entry.date, _ = time.Parse(time.RFC822, gpsfields[5][1:]) //todo pull out just the date component and format
+	entry.fix = gpsfields[6][1:] == "true"
+	entry.ID = gpsfields[7]
+
+	fmt.Printf("Message %s Lat %s Long %s speed %d heading %f fix %t date %s time %s id %s\n",
+		entry.message,
+		entry.latitude,
+		entry.longitude,
+		entry.speed,
+		entry.heading,
+		entry.fix,
+		entry.date,
+		entry.ID)
+
+	go logEntry(db, &entry) //save to database
+	go notifyHTTP(&entry)   //notify any HTTP observers
+
+	conn.WriteToUDP([]byte("OK"), addr)
+	fmt.Printf("Responded to %s\n", addr)
+	conn.Close()
+
+}
