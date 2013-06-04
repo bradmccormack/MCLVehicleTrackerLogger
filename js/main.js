f462d1b30401f315ba4d0c572b8fa51b1d5b3ee6
Brad McCormack <bmccormack@ontech.com.au>
2013-06-04 12:45:24 +1000

    - Putting in login form

--- js/main.js
+++ js/main.js
@@ -17,7 +17,6 @@ var BingMaps = (function(Latitude, Longitude, Zoom, DivID) {
 
 var GoogleMaps = (function(Latitude, Longitude, Zoom, DivID) {
 
-
     //var mapTypes = { MapTypeId.ROADMAP, MapTypeId.SATELLITE, MapTypeId.HYBRID, MapTypeId.TERRAIN }
     var map;
     var divid;
@@ -246,6 +245,26 @@ var System = (function(){
 var mapAPI = {};
 
 $(document).ready(function() {
+
+    $('#myModal').on("shown", function()
+    {
+
+        $(".username").focus(function() {
+            $(".user-icon").css("left","-48px");
+        });
+        $(".username").blur(function() {
+            $(".user-icon").css("left","0px");
+        });
+
+        $(".password").focus(function() {
+            $(".pass-icon").css("left","-48px");
+        });
+        $(".password").blur(function() {
+            $(".pass-icon").css("left","0px");
+        });
+    });
+
+    $('#myModal').modal('toggle');
 
     var system = new System();
     system.init();
