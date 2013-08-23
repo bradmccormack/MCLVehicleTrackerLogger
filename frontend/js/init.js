bef00e9c57daa476e76745a4098bf067150cf72e
Brad mccormack <bradmccormack100@gmail.com>
2013-08-22 21:19:38 -0700

    - Add animate.css and add it to menu for now

--- frontend/js/init.js
+++ frontend/js/init.js
@@ -1,3 +1,12 @@
 //This will replace the old ugly main.js and all seperated logic will go into various controllers
 var app = angular.module('myClubLink', []);
 
+/*
+$(document).ready(function(){
+    $("div#Mainnav ul li").mouseover(function(){
+        $(this).addClass("bounceIn");
+    });
+})
+*/
+
+
