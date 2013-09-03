45d4086ebdc28dadfdb3f06230d831c5a4b3221c
Brad mccormack <bradmccormack100@gmail.com>
2013-09-03 00:52:09 -0700

    - Almost finished updating the Support view/controller

---
+++ frontend/js/Controllers/supportController.js
@@ -0,0 +1,10 @@
+angular.module('myApp.controllers').controller("supportController", ['$scope', function($scope){
+   $scope.User = {
+        First : "Brad",
+        Last: "Mccormack"
+   }
+   
+   $scope.Company = "Test Company";
+   
+   
+}]);
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -8,5 +8,6 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl2'});
     $routeProvider.when('/settings', {templateUrl: 'partials/settings.html', controller: 'settingsController'})
+    $routeProvider.when('/support', {templateUrl: 'partials/support.html', controller: 'supportController'})
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
