42310c4fac796deccb555fda563c8290b4a172d1
Brad mccormack <bradmccormack100@gmail.com>
2013-09-03 19:56:41 +1000

    - Remove dormant controller.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -4,9 +4,6 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl2'});
     $routeProvider.when('/settings', {templateUrl: 'partials/settings.html', controller: 'settingsController'})
     $routeProvider.when('/support', {templateUrl: 'partials/support.html', controller: 'supportController'})
     $routeProvider.when('/license', {templateUrl: 'partials/license.html', controller: 'licenseController'})
