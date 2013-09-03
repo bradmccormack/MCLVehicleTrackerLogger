d1be6b671550d93c300cb9949115c5237b5233d9
Brad mccormack <bradmccormack100@gmail.com>
2013-09-03 01:20:41 -0700

    -Restoring JS and Wysywig editor for support

--- frontend/js/Controllers/supportController.js
+++ frontend/js/Controllers/supportController.js
@@ -1,10 +1,22 @@
 angular.module('myApp.controllers').controller("supportController", ['$scope', function($scope){
-   $scope.User = {
+   
+   function Init() {
+        $("div#editor").wysiwyg();
+   }
+   
+   $scope.User =
+   {
         First : "Brad",
         Last: "Mccormack"
    }
    
    $scope.Company = "Test Company";
+   $scope.Send = function() {
+ 
+   }
+   
+   Init();
+   
    
    
 }]);
