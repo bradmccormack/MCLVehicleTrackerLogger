fb2f070b5e85fd0bbfb1f22492ba6883ab76d584
Brad mccormack <bradmccormack100@gmail.com>
2013-09-17 02:08:45 -0700

    - Hooking up support view

--- frontend/js/Controllers/supportController.js
+++ frontend/js/Controllers/supportController.js
@@ -1,7 +1,17 @@
-angular.module('myApp.controllers').controller("supportController", ['$scope', function($scope){
+angular.module('myApp.controllers').controller("supportController", ['$scope', 'shellService', function($scope, shellService){
    
    $scope.Init = function(){
-        $("div#editor").wysihtml5();
+	   $('#editor').wysiwyg({
+		   hotKeys: {
+			   'ctrl+b meta+b': 'bold',
+			   'ctrl+i meta+i': 'italic',
+			   'ctrl+u meta+u': 'underline',
+			   'ctrl+z meta+z': 'undo',
+			   'ctrl+y meta+y meta+shift+z': 'redo'
+		   }
+	   });
+	   $scope.Subject="";
+	   $scope.Body = "";
    }
    
    $scope.User =
@@ -14,6 +24,7 @@ angular.module('myApp.controllers').controller("supportController", ['$scope', f
    $scope.Send = function() {
  
    }
+
    
 
 
