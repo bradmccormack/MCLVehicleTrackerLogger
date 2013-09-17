b1f94e38231f4ba83f4fb62850cb05097057c8e7
Brad mccormack <bradmccormack100@gmail.com>
2013-09-17 04:30:05 -0700

    - Cleanup of structure and hookup login directive

--- frontend/js/directives.js
+++ frontend/js/directives.js
@@ -8,4 +8,27 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }]).directive('authapplication', function() {
+		return {
+			restrict: 'C',
+			link: function(scope, elem, attrs) {
+				//once Angular is started, remove class:
+				elem.removeClass('waiting-for-angular');
+
+				var login = elem.find('#login-holder');
+				var main = elem.find('#content');
+
+				login.hide();
+
+				scope.$on('event:auth-loginRequired', function() {
+					login.slideDown('slow', function() {
+						main.hide();
+					});
+				});
+				scope.$on('event:auth-loginConfirmed', function() {
+					main.show();
+					login.slideUp();
+				});
+			}
+		}
+	});
