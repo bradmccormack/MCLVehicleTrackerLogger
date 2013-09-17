4b0fd23b583fc191675b394f2b234ef9d4b9c6a0
Brad mccormack <bradmccormack100@gmail.com>
2013-09-17 03:58:34 -0700

    - Before hooking up AngularJS authentiction

--- frontend/js/Controllers/mainController.js
+++ frontend/js/Controllers/mainController.js
@@ -1,22 +1,51 @@
-angular.module('myApp.controllers').controller("mainController", ['$scope', 'networkService', function($scope, networkService){
-   
-   function Login() {
-     
-     var cookies = $.cookie();
-        if("session" in $.cookie())
-        {
-                if("success" in cbobj && typeof cbobj.success == "function") {
-                        cbobj.success("Craig Smith"); //TODO pull the name from the session cookie
-                }
-                return;
-        }
-	    networkService.Init();
-   }
+angular.module('myApp.controllers').controller("mainController", ['$scope', 'networkService', 'shellService', '$http', function ($scope, networkService, shellService, $http) {
 
-   $scope.SystemInit = function() {
-       Login();
-   }
-   
-   
-   
+
+	function BindAnim()
+	{
+		//Login
+		$('#myModal').on("shown", function()
+		{
+			$(".username").focus(function() {
+				$(".user-icon").css("left","-48px");
+			});
+			$(".username").blur(function() {
+				$(".user-icon").css("left","0px");
+			});
+
+			$(".password").focus(function() {
+				$(".pass-icon").css("left","-48px");
+			});
+			$(".password").blur(function() {
+				$(".pass-icon").css("left","0px");
+			});
+		});
+	}
+
+	function Login() {
+
+		var cookies = $.cookie();
+		if ("session" in $.cookie()) {
+
+			//set the properties on the user object etc in the shellService
+
+		}
+		else
+		{
+			//$http({method: "POST"})
+		}
+		networkService.Init();
+		/*
+		var myModal = $("#myModal");
+		$("#myModal").toggle();
+		*/
+
+	}
+
+	$scope.SystemInit = function () {
+		BindAnim();
+		Login();
+	}
+
+
 }]);
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
 config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'mainController'})
   $routeProvider.when('/settings', {templateUrl: 'partials/settings.html', controller: 'settingsController'})
   $routeProvider.when('/support', {templateUrl: 'partials/support.html', controller: 'supportController'})
   $routeProvider.when('/license', {templateUrl: 'partials/license.html', controller: 'licenseController'})
