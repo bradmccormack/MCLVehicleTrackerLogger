be8dc4036038d5a89fbdc6170b838e3d4dd10c74
Brad mccormack <bradmccormack100@gmail.com>
2013-10-18 22:34:21 -0700

    - Making notes about lack of security and cookies

--- frontend/js/Controllers/loginController.js
+++ frontend/js/Controllers/loginController.js
@@ -44,6 +44,9 @@ var LoginCtrl = angular.module('myApp.controllers').controller("loginController"
 LoginCtrl.Login = function($q, $http, $location, shellService, networkService) {
 
     var defer = $q.defer();
+
+    //The following needs refining. If a malicious user was to inject a cookie called data it would log in. It would be erroneous, but it would still try.
+    //Instead we still need to hit the server with the Post login regardless to confirm the data cookie is valid
     if ("data" in $.cookie()) {
 
         defer.reject("User logged in already");
