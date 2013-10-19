3a1eba99f00b0a49e996d18ed83e233b308f1ee1
Brad mccormack <bradmccormack100@gmail.com>
2013-10-18 20:28:01 -0700

    - Add a resolve property on the Login Route so if the cookie is already set then just return a rejected promise so the view and controller isn't instantiated and then redirect to tracking otherwise return a resolved promise so login can commence

--- frontend/js/Controllers/loginController.js
+++ frontend/js/Controllers/loginController.js
@@ -1,7 +1,8 @@
-angular.module('myApp.controllers').controller("loginController", ['$scope', '$cookieStore', '$http', 'shellService', 'authService', 'networkService', '$location',
+var LoginCtrl = angular.module('myApp.controllers').controller("loginController", ['$scope', '$cookieStore', '$http', 'shellService', 'authService', 'networkService', '$location',
 	function ($scope, $cookieStore, $http, shellService, authService, networkService, $location) {
 
 
+        /*bind event handlers using jquery to do the animation */
 		$(".username").focus(function() {
 			$(".user-icon").css("left","-48px");
 		});
@@ -17,31 +18,59 @@ angular.module('myApp.controllers').controller("loginController", ['$scope', '$c
 		});
 
 
-		$scope.Login = function () {
 
-			if ("data" in $.cookie()) {
-				$http({method: "GET", url: "/system/login", withCredentials: true}).
-					success(function (data, status, headers, config) {
-						shellService.LoadConfig(data);
-						networkService.Init();
-						$location.path("/tracking");
-					})
-			}
-			else {
-				$http({method: 'POST', url: '/system/login', headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-					withCredentials: true, data: $.param({name: $scope.Username, password: $scope.Password})}).
-					success(function (data, status, headers, config) {
-						shellService.LoadConfig(data);
-						networkService.Init();
-						$location.path("/tracking");
-						//authService.loginConfirmed(); //Login confirmed so the authservice will broadcast auth event which the directive will take care of and close login etc
+        $scope.Login = function() {
+            $http({method: 'POST', url: '/system/login', headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                withCredentials: true, data: $.param({name: $scope.Username, password: $scope.Password})}).
+                success(function (data, status, headers, config) {
+                    shellService.LoadConfig(data);
+                    networkService.Init();
+                    $location.path("/tracking");
+                    authService.loginConfirmed(); //Login confirmed so the authservice will broadcast auth event which the directive will take care of and close login etc
 
-					}).
-					error(function (data, status, headers, config) {
+                }).
+                error(function (data, status, headers, config) {
 
-					});
-			}
-		}
+                });
+        }
+
+
 
 
 	}]);
+
+
+//the $q library is the promise library
+LoginCtrl.Login = function($q, $http, $location, shellService, networkService) {
+
+    var defer = $q.defer();
+    if ("data" in $.cookie()) {
+
+        defer.reject("User logged in already");
+
+        $http({method: "GET", url: "/system/login", withCredentials: true}).
+            success(function (data, status, headers, config) {
+                shellService.LoadConfig(data);
+                networkService.Init();
+                $location.path("/tracking");
+            })
+    }
+    else
+    {
+        $http({method: 'POST', url: '/system/login', headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            withCredentials: true, data: $.param({name: '', password: ''})}).
+            success(function (data, status, headers, config) {
+                shellService.LoadConfig(data);
+                networkService.Init();
+                $location.path("/tracking");
+                authService.loginConfirmed(); //Login confirmed so the authservice will broadcast auth event which the directive will take care of and close login etc
+
+            }).
+            error(function (data, status, headers, config) {
+
+            });
+        defer.resolve();
+
+    }
+    return defer.promise;
+}
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -4,13 +4,29 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
 config(['$routeProvider', function($routeProvider) {
 
-  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginController'})
+  $routeProvider.when('/login',
+      {
+          templateUrl: 'partials/login.html',
+          controller: 'loginController',
+          resolve:  {
+              /*
+              the resolve property will be called before the controller is instantiated and the view is loaded
+
+               */
+              //$
+              loadData: LoginCtrl.Login
+          }
+      });
+
+
+
+
   $routeProvider.when('/settings', {templateUrl: 'partials/settings.html', controller: 'settingsController'})
   $routeProvider.when('/support', {templateUrl: 'partials/support.html', controller: 'supportController'})
   $routeProvider.when('/license', {templateUrl: 'partials/license.html', controller: 'licenseController'})
   $routeProvider.when('/reports', {templateUrl: 'partials/report.html', controller: 'reportController'})
   $routeProvider.when('/tracking', {templateUrl: 'partials/tracking.html', controller: 'trackingController'})
-  $routeProvider.otherwise({redirectTo: '/tracking'});
+  $routeProvider.otherwise({redirectTo: '/login'});
 
 
 }]);
