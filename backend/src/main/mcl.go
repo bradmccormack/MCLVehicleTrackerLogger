f54c11c749716c5ecaa1b1d8d492c281d0940892
Brad mccormack <bradmccormack100@gmail.com>
2014-01-13 20:35:43 -0800

    - Make sure companysettings are created on sample data

--- backend/src/main/mcl.go
+++ backend/src/main/mcl.go
@@ -700,9 +700,9 @@ func createDb() {
 		`INSERT INTO CompanySettings (CompanyID, RadioCommunication, DataCommunication, SecurityRemoteAdmin,
 		SecurityConsoleAccess, SecurityAdminPasswordReset, MobileSmartPhoneAccess, MinZoom, MaxZoom, HistoricalmapsKmMin, ClubBoundaryKM)
 		VALUES
-		(1, 1, 1, 0, 0, 0, 0, 1, 10, 10, 100);
+		(1, 1, 1, 0, 0, 0, 0, 1, 10, 10, 100);`,
 
-		INSERT INTO CompanySettings (CompanyID, RadioCommunication, DataCommunication, SecurityRemoteAdmin,
+		`INSERT INTO CompanySettings (CompanyID, RadioCommunication, DataCommunication, SecurityRemoteAdmin,
 		SecurityConsoleAccess, SecurityAdminPasswordReset, MobileSmartPhoneAccess, MinZoom, MaxZoom, HistoricalmapsKmMin, ClubBoundaryKM)
 		VALUES
 		(2, 1, 1, 0, 0, 0, 0, 1, 10, 10, 100);`,
